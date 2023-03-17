77d5bdf4538de2e9d6d08c263923faea8abc6846
Salami1606 <salamijelili06@gmail.com>
2023-03-17 10:36:51 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,11 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		print(" % d is zero\n", n);
+		print("%d is zero\n", n);
 	}
 	else
 	{
-		print(" % d is negative\n", n);
+		print("%d is negative\n", n);
 	}
 	return (0);
 }
