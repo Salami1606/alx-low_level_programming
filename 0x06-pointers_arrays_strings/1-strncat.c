1a4c389f4182a69fa6e49a10bce85c83e6996a70
Salami1606 <salamijelili06@gmail.com>
2023-03-30 10:41:07 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,9 +19,9 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	}
 	j = 0;
-	while (j < n && src[j] != '\n')
+	while (j < n && src[j] != '\0')
 	{
-		dest[i] = src[i];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
