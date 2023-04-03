516a556949823a3ca467e5e071d0de19ec8b9c01
Salami1606 <salamijelili06@gmail.com>
2023-04-03 16:15:35 -0700

    The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio>
+#include <stdio.h>
 /**
  * print_diagsums - Entry point
  * @a: input
@@ -17,11 +17,9 @@ void print_diagsums(int *a, int size)
 	{
 		sum1 = sum1 + a[y * size + y];
 	}
-
 	for (y = size - 1; y >= 0; y--)
 	{
 		sum2 += a[y * size + (size - y - 1)];
 	}
-
 	printf("%d, %d\n", sum1, sum2);
 }
